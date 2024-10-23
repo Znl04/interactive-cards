dea276e9ab48396f042e3ec7ce8852c31af4fa53
Nurzhan Zeinolla <zeinolla.nur04@gmail.com>
2024-10-23 21:28:57 +0500

    refac: updated code

--- mini-game/app.js
+++ mini-game/app.js
@@ -6,24 +6,25 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement('div');
   square.classList.add("square");
 
-  square.addEventListener("mouseover", () => setColor(square))
-  square.addEventListener("mouseleave", () => removeColor(square))
+  square.addEventListener("mouseover", setColor)
+  square.addEventListener("mouseleave", removeColor)
 
   board.append(square);
 }
 
-function setColor(element) {
+function setColor(e) {
+  element = e.target;
   const color = getRandomColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
+function removeColor(e) {
+  element = e.target;
   element.style.backgroundColor = "#1d1d1d"
   element.style.boxShadow = `0 0 2px #1d1d1d`
 }
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
